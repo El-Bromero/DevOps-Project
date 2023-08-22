98d5c7aa858ff6296d0e368f51681907c2badae9
Brandon Romero <brandonaromero@gmail.com>
2023-08-22 11:26:57 -0500

    Added .gitignore for .terraform files. Added s3 remote backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
+  region   = var.aws_region
+  profile  = var.aws_profile
 }
 
 terraform {
-    
+  backend "s3" {
+    bucket = "bromero-projects"
+    key    = "bromero/devops-project/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
