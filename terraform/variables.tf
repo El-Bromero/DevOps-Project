05848ea80c7c990e549ff8e16f848780cd4b5668
Brandon Romero <brandonaromero@gmail.com>
2023-08-22 10:48:05 -0500

    Added main and variables tf files

---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  description = "Enter a region id to deploy infrastructure into"
+}
+
+variable "aws_profile" {
+  description = "Enter a profile name to deploy infrastructure into"
+}
